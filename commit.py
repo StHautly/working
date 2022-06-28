712ca90941d2a3a56a5b17381a444eb358ea8cf8
hautly <tupalevi1@gmail.com>
2022-06-28 13:57:34 -0300

    commit print X

--- commit.py
+++ commit.py
@@ -3,8 +3,10 @@ from datetime import datetime as dt
 import os
 
 x = dt.now().isoformat()
+print(x)
 
-host = r"C:\Users\Administrador\github\working"
-repo = Repo(host)
-repo.git.add(update=True)
-repo.index.commit(x)
+#
+# host = r"C:\Users\Administrador\github\working"
+# repo = Repo(host)
+# repo.git.add(update=True)
+# repo.index.commit(x)
