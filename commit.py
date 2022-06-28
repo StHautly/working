99506d73b1c30fea9b5ea3a42104a9ec7b58b2d5
hautly <tupalevi1@gmail.com>
2022-06-28 14:12:26 -0300

    2022-06-28T14:12:26.549489

--- commit.py
+++ commit.py
@@ -4,10 +4,17 @@ import os
 
 x = dt.now().isoformat()
 
+
 host = r"C:\Users\Administrador\github\working"
-repo = Repo(host)
-repo.git.checkout('origin')
-repo.git.add('*')
-repo.index.commit(x)
-repo.git.push()
-print('OK')
+def git_push():
+    try:
+        repo = Repo(host)
+        repo.git.add(update=True)
+        repo.index.commit(x)
+        origin = repo.remote(name='origin')
+        origin.pull()
+        origin.push()
+    except:
+        print('Some error occured while pushing the code')
+
+git_push()
